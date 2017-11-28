506e6deab3f8bf3216c90da218d1917022358e52
andrius <spam@dwm.lt>
2017-11-28 23:25:35 +0200

    DRLT band for X7/X9

--- src/SCRIPTS/BF/X9/vtx.lua
+++ src/SCRIPTS/BF/X9/vtx.lua
@@ -10,12 +10,13 @@ return {
     minBytes       = 5,
     text = {},
     fields = {
-        { t = "Band",    x = 25,  y = 14, sp = 50, min=1, max=5, vals = { 2 }, to = SMLSIZE, table = { "A", "B", "E", "F", "R" }, upd = function(self) self.updateVTXFreq(self) end },
+        { t = "Band",    x = 25,  y = 14, sp = 50, min=1, max=6, vals = { 2 }, to = SMLSIZE, table = { "A", "B", "E", "F", "R", "DRLT" }, upd = function(self) self.updateVTXFreq(self) end },
         { t = "Channel", x = 25,  y = 24, sp = 50, min=1, max=8, vals = { 3 }, to = SMLSIZE, upd =  function(self) self.updateVTXFreq(self) end },
         { t = "Power",   x = 25,  y = 34, sp = 50, min=1, vals = { 4 }, to = SMLSIZE, upd = function(self) self.updatePowerTable(self) end },
         { t = "Pit",     x = 25,  y = 44, sp = 50, min=0, max=1, vals = { 5 }, to = SMLSIZE, table = { [0]="OFF", "ON" } },
         { t = "Dev",     x = 100, y = 14, sp = 32, write = false, ro = true, vals = { 1 }, to = SMLSIZE, table = {[3]="SmartAudio",[4]="Tramp",[255]="None"} },
         { t = "Freq",    x = 100, y = 24, sp = 32, min=5000, max=6000, to = SMLSIZE, ro=true },
+        { t = "ChanID",  x = 100, y = 34, sp = 32, min=5000, max=6000, to = SMLSIZE, ro=true },
     },
     freqLookup = {
         { 5865, 5845, 5825, 5805, 5785, 5765, 5745, 5725 }, -- Boscam A
@@ -23,15 +24,34 @@ return {
         { 5705, 5685, 5665, 5645, 5885, 5905, 5925, 5945 }, -- Boscam E
         { 5740, 5760, 5780, 5800, 5820, 5840, 5860, 5880 }, -- FatShark
         { 5658, 5695, 5732, 5769, 5806, 5843, 5880, 5917 }, -- RaceBand
+        { "5665 E3", "5695 R2", "5840 F6", "5866 B8", "5917 R8", "5945 E8", "N/A", "N/A" }, -- DRLT band
     },
     postLoad = function (self)
         if self.values[2] == 0 or self.values[3] == 0 or self.values[4] == 0 then
             self.values = {}
         end
+
+        local channel = (self.values[2]-1)*8 + self.values[3]-1
+        if channel == 18 then self.fields[1].value = 6; self.fields[2].value = 1; self.values[2] = 6; self.values[3] = 1
+          elseif channel == 33 then self.fields[1].value = 6; self.fields[2].value = 2; self.values[2] = 6; self.values[3] = 2
+          elseif channel == 29 then self.fields[1].value = 6; self.fields[2].value = 3; self.values[2] = 6; self.values[3] = 3
+          elseif channel == 15 then self.fields[1].value = 6; self.fields[2].value = 4; self.values[2] = 6; self.values[3] = 4
+          elseif channel == 39 then self.fields[1].value = 6; self.fields[2].value = 5; self.values[2] = 6; self.values[3] = 5
+          elseif channel == 23 then self.fields[1].value = 6; self.fields[2].value = 6; self.values[2] = 6; self.values[3] = 6
+        end
     end,
     preSave = function(self)
         local valsTemp = {}
         local channel = (self.values[2]-1)*8 + self.values[3]-1
+        if channel == 40 then channel = 18
+          elseif channel == 41 then channel = 33
+          elseif channel == 42 then channel = 29
+          elseif channel == 43 then channel = 15
+          elseif channel == 44 then channel = 39
+          elseif channel == 45 then channel = 23
+          elseif channel == 46 then channel = 23
+          elseif channel == 47 then channel = 23
+        end
         valsTemp[1] = bit32.band(channel,0xFF)
         valsTemp[2] = bit32.rshift(channel,8)
         valsTemp[3] = self.values[4]
@@ -53,6 +73,7 @@ return {
         if (#(self.values) or 0) >= self.minBytes then
             if (self.fields[2].value or 0) > 0 and (self.fields[3].value or 0) > 0 then
                 self.fields[6].value = self.freqLookup[self.values[2]][self.values[3]]
+                self.fields[7].value = (self.values[2]-1)*8 + self.values[3]-1
             end
         end
     end
